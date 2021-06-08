792e3e395122f2ece32f14cee6b50437552e20ed
Farah Mansour <f.mansour@web.de>
2021-06-08 21:43:36 +0200

    added navigation icons

--- src/FooterNavigation.js
+++ src/FooterNavigation.js
@@ -1,28 +1,37 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
+import today from './images/today.svg';
+import plus from './images/plus.svg';
+import goals from './images/goals.svg';
+ 
 
 
 export default function FooterNavigation() {
     return (
     <NavWrapper>
         <NavLink exact to="/" className="link">
-            today
+            <img src={today} alt='Today page' />
         </NavLink>
         <NavLink to="/add-goal" className="link">
-            plus
+         <img src={plus} alt='Adds a new entry' />
         </NavLink>
         <NavLink to="/goals" className="link">
-            goals
+         <img src={goals} alt='Goals page' />
         </NavLink>
      </NavWrapper>
     )
 }
 
 const NavWrapper = styled.footer`
+  background-color: bananamania;
   bottom: 0;
   display: flex;
   justify-content: space-around;
   position: fixed;
   width: 100%;
+
+ img {
+    height: 3rem;
+  }
 `
