44bcf3c7e40bf534f55daf74aa4ed74bceebb816
Farah Mansour <f.mansour@web.de>
2021-06-08 11:51:20 +0200

    added GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+body {
+background-color: hotpink;
+background-repeat: no-repeat;
+background-size: cover;
+}
+`;
