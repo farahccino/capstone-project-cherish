3fc7c189ca2e76c3962083b1a8b451dd822216ea
Farah Mansour <80906106+farahccino@users.noreply.github.com>
2021-06-11 19:19:25 +0200

    added habit form

---
+++ src/components/HabitForm.js
@@ -0,0 +1,136 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import styled from 'styled-components/macro';
+
+export default function HabitForm({
+    headlineText,
+    onAddHabit,
+    häufigkeit,
+    habitToEdit,
+}) { 
+const initialState = {
+    ziel: '',
+    häufigkeit: '',
+};
+
+const [habit, setHabit] = useState(habitToEdit ?? initialState);
+const [isError, setIsError] = useState(false);
+
+function updateHabit(event) {
+    const fieldName = event.target.name;
+    let fieldValue = event.target.value;
+   
+    setHabit({ ...habit, [fieldName]: fieldValue });
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    if (validateHabit(habit)) {
+      onAddHabit(habit);
+      setHabit(initialState);
+      setIsError(false);
+    } else {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 4000);
+    }
+  }
+   
+
+
+  
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <h2>{headlineText}</h2>
+      <ErrorBox data-testid="form-error-display" isError={isError}>
+        <p>You have an error in your form.</p>
+      </ErrorBox>
+      <label htmlFor="habitName">Habit Name</label>
+      <input
+        type="text"
+        name="ziel"
+        onChange={updateHabit}
+        value={habit.ziel}
+      />
+     
+     <label htmlFor="häufigkeit">Häufigkeit</label>
+      <select name="häufigkeit" id="häufigkeit" onChange={updateHabit} value={habit.häufigkeit}>
+        <option value="">---Please select ---</option>
+        {häufigkeit.map((häufigkeit) => (
+          <option key={häufigkeit._id} value={häufigkeit.name}>
+            {häufigkeit.name}
+          </option>
+        ))}
+      </select>
+
+      <Button isPrimary>hinzufügen</Button>
+      <Button type="reset" onClick={() => setHabit(initialState)}>
+        abbrechen
+      </Button>
+    </Form>
+  );
+}
+
+
+const Form = styled.form`
+  display: grid;
+  gap: 0.5rem;
+  margin: 0 auto;
+  max-width: 25rem;
+  label,
+  legend {
+    font-weight: bold;
+    span {
+      font-weight: normal;
+    }
+  }
+  legend {
+    margin-bottom: 0.5rem;
+    padding: 0;
+  }
+  input,
+  select {
+    padding: 0.5rem;
+    margin-bottom: 0.3rem;
+  }
+  fieldset {
+    border: none;
+    display: flex;
+    gap: 0.4rem;
+    padding: 0;
+    margin: 0;
+  }
+  fieldset > label {
+    font-weight: normal;
+  }
+  input[type='radio'],
+  input[type='checkbox'] {
+    transform: scale(1.5);
+    margin-right: 0.5rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1.5rem;
+  border-radius: 0.4rem;
+  border: none;
+  background: ${(props) =>
+    props.isPrimary ? 'hsl(160, 60%, 50%)' : 'hsla(160, 60%, 80%, 0.3)'};
+  cursor: pointer;
+  font-weight: ${(props) => (props.isPrimary ? '600' : '100')};
+  font-size: 1.2rem;
+`;
+
+
+const ErrorBox = styled.div`
+  background: hsl(340, 60%, 50%);
+  color: hsl(340, 95%, 95%);
+  padding: ${(props) => (props.isError ? '1.2rem' : 0)};
+  border-radius: 0.5rem;
+  opacity: ${(props) => (props.isError ? 1 : 0)};
+  max-height: ${(props) => (props.isError ? '100%' : '1px')};
+  transition: all 1s ease-in-out;
+  font-size: ${(props) => (props.isError ? '1rem' : '1px')};
+  font-weight: bold;
+`;
--- src/pages/Plus.js
+++ src/pages/Plus.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import HabitForm from '../components/HabitForm';
 import FooterNavigation from '../components/FooterNavigation';
 
 export default function Plus() {
   return (
     <>
      <h1> This is the plus page! </h1>
+     <HabitForm />
      <FooterNavigation />
     </>
   );
