e52d345f32569e17eae03173af311ec39e3cb656
Farah Mansour <80906106+farahccino@users.noreply.github.com>
2021-06-21 17:41:53 +0200

    changes after code review

--- src/components/HabitForm.js
+++ src/components/HabitForm.js
@@ -35,7 +35,7 @@ export default function HabitForm({
   const placeholderText = `neues Ziel tippen...
 `;
 
-  function updateHabit(event) {
+  function handleUpdateHabit(event) {
     const fieldName = event.target.name;
     let fieldValue = event.target.value;
     setHabit({ ...habit, [fieldName]: fieldValue });
@@ -62,7 +62,7 @@ export default function HabitForm({
       <Ziel
         type="text"
         name="ziel"
-        onChange={updateHabit}
+        onChange={handleUpdateHabit}
         value={habit.ziel}
         placeholder={placeholderText}
       />
@@ -70,7 +70,7 @@ export default function HabitForm({
       <select
         name="häufigkeit"
         id="häufigkeit"
-        onChange={updateHabit}
+        onChange={handleUpdateHabit}
         value={habit.häufigkeit}
       >
         <option value=""> wähle die Häufigkeit </option>
--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -1,13 +1,8 @@
 export function updateLocalStorage(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-  }
-  
-  export function loadFromLocalStorage(key) {
-    try {
-      const localData = localStorage.getItem(key);
-      return JSON.parse(localData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function loadFromLocalStorage(key) {
+  const data = JSON.parse(localStorage.getItem(key)) || [];
+  return data;
+}
